e77e62d663b9e1bcef9e64c300db1f58d8b28e01
Ryan Fanchiotti <ryan@rur.com>
2025-06-01 12:03:23 -0400

    added vscode config

--- ryan-programs/default.nix
+++ ryan-programs/default.nix
@@ -3,6 +3,7 @@
     ./btop.nix
     ./tools-libs.nix
     ./fzf.nix
+    ./vscode.nix
     ./bash
     ./wezterm
   ];
---
+++ ryan-programs/vscode.nix
@@ -0,0 +1,92 @@
+{ pkgs, ... }:
+
+{
+  # Visual Studio Code setup
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode.fhs;
+
+    # This property will be used to generate settings.json:
+    # https://code.visualstudio.com/docs/getstarted/settings#_settingsjson
+    profiles.default.userSettings = {
+      diffEditor.ignoreTrimWhitespace = false;
+
+      search.followSymlinks = false;
+
+      extensions.ignoreRecommendations = true;
+      extensions.autoUpdate = false;
+
+      telemetry.telemetryLevel = "off";
+
+      files.trimFinalNewLines = true;
+
+      security.workspace.trust.untrustedFiles = "open";
+      security.workspace.trust.enabled = false;
+
+      debug.toolBarLocation = "commandCenter";
+
+      git.openRepositoryInParentFolders = "never";
+
+      terminal.integrated = {
+        fontSize = 17;
+        cursorBlinking = false;
+        minimumContrastRatio = false;
+        shellIntegration = {
+          history = 0;
+          enabled = false;
+        };
+        gpuAcceleration = "on";
+        enablePersistentSessions = false;
+      };
+
+      breadcrumbs.enabled = false;
+
+      editor = {
+        smoothScrolling = false;
+        cursorBlinking = "solid";
+        bracketPairColorization = false;
+        hover.delay = 50;
+        inlayHints.enabled = "off";
+        wordWrap = "on";
+        fontSize = 17;
+        fontFamily = "Iosevka";
+        cursorStyle = "line";
+        folding = false;
+        stickyScroll.enabled = false;
+        renderLineHighlight = "none";
+      };
+
+      explorer = {
+        decorations.colors = false;
+        sortOrder = "type";
+      };
+
+      workbench = {
+        editor.showIcons = false;
+        editor.tabSizing = "shrink";
+        statusBar.visible = false;
+        tree.indent = 10;
+        iconTheme = null;
+        startupEditor = "none";
+        settings.applyToAllProfiles = ["editor.fontSize"];
+        colorTheme = "Solarized Light";
+      };
+
+      window.restoreWindows = "none";
+      window.title = "\${activeFolderShort}";
+    };
+
+    # See https://code.visualstudio.com/docs/getstarted/keybindings#_advanced-customization
+    profiles.default.keybindings = [
+      {
+          key = "shift+cmd+j";
+          command = "workbench.action.focusActiveEditorGroup";
+          when = "terminalFocus";
+      }
+    ];
+
+    profiles.default.extensions = with pkgs.vscode-marketplace; [
+      
+    ];
+  };
+}
