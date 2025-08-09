6f0b9d66995efbb72e3f48f73d86cf38400abaa8
Ryan Fanchiotti <ryan@rur.com>
2025-08-09 01:04:55 -0400

    change hostname

--- linux/system-setup.nix
+++ linux/system-setup.nix
@@ -29,7 +29,7 @@
 
   networking = {
     # Define hostname
-    hostName = "desktop-nixos";
+    hostName = "piplup";
 
     # Enable networking through network manager
     networkmanager.enable = true;
